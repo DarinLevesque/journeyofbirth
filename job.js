578d7d4ce688e574cc0710a17e33a97a05390e24
Darin Levesque <darin@darinlevesque.com>
2016-07-09 20:53:13 -0400

    added html template

--- job.js
+++ job.js
@@ -3,29 +3,52 @@ var app = express();
 var fortune = require('./lib/fortune.js');
 
 // set up handlebars view engine
-var handlebars = require('express3-handlebars')
+var handlebars = require('express-handlebars')
     .create({
         defaultLayout: 'main'
     });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+// Assign Port
 app.set('port', process.env.PORT || 3000);
 
+// remove header venurability
 app.disable('x-powered-by');
 
+// set static folder
 app.use(express.static(__dirname + '/public'));
 
+// ignore tests in production
 app.use(function(req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' &&
         req.query.test === '1';
     next();
 });
+
+switch(app.get('env')){
+ case 'development':
+ // compact, colorful dev logging
+ app.use(require('morgan')('dev'));
+ break;
+ case 'production':
+ // module 'express-logger' supports daily log rotation
+ app.use(require('express-logger')({
+ path: __dirname + '/log/requests.log'
+ }));
+ break;
+}
+
+
 // routes go here....
 
 app.get('/', function(req, res) {
-    res.render('home');
+    res.render('comingsoon', { layout: null });
 });
+// app.get('/', function(req, res) {
+//     res.render('home');
+// });
+
 app.get('/about', function(req, res) {
     res.render('about', {
         fortune: fortune.getFortune(),
@@ -43,7 +66,10 @@ app.use(function(err, req, res, next) {
     res.status(500);
     res.render('500');
 });
+
+// start node server
 app.listen(app.get('port'), function() {
-    console.log('Express started on http://localhost:' +
-        app.get('port') + '; press Ctrl-C to terminate.');
+    console.log('Express started in ' + app.get('env') +
+ ' mode on http://localhost:' + app.get('port') +
+ '; press Ctrl-C to terminate.');
 });
